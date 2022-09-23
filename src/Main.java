e2e2e83347c9b5acb86ba09bf71daade77a99182
Anastasia <atomuz1957@bths.edu>
2022-09-22 20:09:22 -0400

    fin project from home

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ import java.text.DecimalFormat;
 
 public class Main {
     public static void main(String[] args){
+        //set ups
         Scanner scan = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("#.##");
+
+        //intro stuff
         System.out.println("Welcome to the tip calculator");
         System.out.print("How many people are in your group? ");
         int numPeople = scan.nextInt();
@@ -11,17 +15,22 @@ public class Main {
         System.out.print("What's the tip percentage? (0-100): ");
         int tipPercentage = scan.nextInt();
         scan.nextLine();
-        double tipDecimal = (double) tipPercentage / 100;
-        double billNoTip = 0;
 
+        double tipDecimal = (double) tipPercentage / 100; //turn percentage to decimal
+        double billNoTip = 0; //initialize with zero so you can add item costs later
+
+        //prompts user to enter one item cost
         System.out.print("Enter a cost in dollar and cent, e.g. 12.50 (-1 to end): ");
         double newItem = scan.nextDouble();
         scan.nextLine();
-        billNoTip += newItem;
+        billNoTip += newItem; //adds cost of item to total cost
+
+        //loop for user to put in item costs
         while (newItem != -1){
             System.out.print("Enter a cost in dollar and cent, e.g. 12.50 (-1 to end): ");
             newItem = scan.nextDouble();
             scan.nextLine();
+            //if user inputs -1, stop/break out of the loop
             if (newItem == -1){
                 break;
             }
@@ -32,9 +41,25 @@ public class Main {
         double tipAmount = billNoTip * tipDecimal;
         double billWithTip = billNoTip + tipAmount;
         double perPersonNoTip = billNoTip/numPeople;
+        double tipPerPerson = tipAmount / numPeople;
+        double perPersonWithTip = billWithTip / numPeople;
+
+        //formatting
+        String formatTipAmount = formatter.format(tipAmount);
+        String formatBillWithTip = formatter.format(billWithTip);
+        String formatPerPersonNoTip = formatter.format(perPersonNoTip);
+        String formatTipPerPerson = formatter.format(tipPerPerson);
+        String formatPersonWithTip = formatter.format(perPersonWithTip);
 
 
-        System.out.println("user total: " + billNoTip);
+        //printing information
+        System.out.println("Total Bill Before Tip: " + billNoTip);
+        System.out.println("Tip Percentage: " + tipPercentage);
+        System.out.println("Total Tip: " + formatTipAmount);
+        System.out.println("Total Bill with Tip: " + formatBillWithTip);
+        System.out.println("Per Person Cost Before Tip: " + formatPerPersonNoTip);
+        System.out.println("Tip Per Person: " + formatTipPerPerson);
+        System.out.println("Total Cost Per Person: " + formatPersonWithTip);
 
 
 
